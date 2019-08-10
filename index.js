9dfab6ebe7f63d0de250ca35371bad08c1809d81
admin <ts70@evansville.edu>
2019-08-10 11:02:03 -0400

    test commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(require('body-parser').json());
 require('./controllers')(app);
 require('./config/associations');
 
+console.log('this is a test');
+
 app.listen(process.env.PORT, () => {
     console.log(`server is listening on port ${process.env.PORT}`);
 })
