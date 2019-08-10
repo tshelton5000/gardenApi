b5f18d442acbb94503c747c456ea529029c3ce23
admin <ts70@evansville.edu>
2019-08-10 08:16:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+require('./models');
+
+app.use(require('cors')())
+app.use(require('body-parser').json());
+
+require('./controllers')(app);
+require('./config/associations');
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is listening on port ${process.env.PORT}`);
+})
